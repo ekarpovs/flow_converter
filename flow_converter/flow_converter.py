c8db0bb47c68cabd6d3b831159cbdd9ac26359d0
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-10-16 13:50:53 +0300

    Simplify FRFSM defintion

--- flow_converter/flow_converter.py
+++ flow_converter/flow_converter.py
@@ -21,8 +21,8 @@ class FlowConverter():
       state_def = self.templates.def_state([state_name, state_entry_action, state_exit_action, trans_def])
       states_def.append(state_def)
     # Decorate states by stm elements
-    if self.meta_has_statement():
-      states_def = self.decorate(states_def)
+    # if self.meta_has_statement():
+    #   states_def = self.decorate(states_def)
     # TEMPLATE_FSM = ["info", "context-name", "init-action", "first-state", "states"]    
     fsm_def = self.templates.def_fsm(['', '', '', first_state_name, states_def])
     return fsm_def
@@ -38,23 +38,23 @@ class FlowConverter():
     stm_name = state_def.get('name').split('-')[1]
     return stm_name == 'forinrangestm' or stm_name == 'whilestm'
     
-  def decorate(self, states_def):
-    for idx, state_def in enumerate(states_def):
-      if self.state_is_statement(state_def):
-        state_name = state_def.get('name') 
-        stm_name = state_name.split('-')[1]
-        if stm_name == 'forinrangestm':
-          state_def['exit-action'] = '____frfsm-actions.forinrange_exit'
-        if stm_name == 'whilestm':
-          state_def['exit-action'] = '____frfsm-actions.while_exit'
-        next_state_def = states_def[idx+1]
-        next_state_def['exit-action'] = '____frfsm-actions.stm_included'
-        # Change the last state in the stm transition destination
-        step_meta = self.meta[idx]
-        include = step_meta.get('params').get('include')
-        last_state_def_inside_stm = states_def[idx+include]
-        transitions_def = last_state_def_inside_stm.get('transitions')
-        for tr in transitions_def:
-          if tr['event'] == 'next':
-            tr['target'] = state_name
-    return states_def
+  # def decorate(self, states_def):
+  #   for idx, state_def in enumerate(states_def):
+  #     if self.state_is_statement(state_def):
+  #       state_name = state_def.get('name') 
+  #       stm_name = state_name.split('-')[1]
+  #       if stm_name == 'forinrangestm':
+  #         state_def['exit-action'] = '____frfsm-actions.forinrange_exit'
+  #       if stm_name == 'whilestm':
+  #         state_def['exit-action'] = '____frfsm-actions.while_exit'
+  #       next_state_def = states_def[idx+1]
+  #       next_state_def['exit-action'] = '____frfsm-actions.stm_included'
+  #       # Change the last state in the stm transition destination
+  #       step_meta = self.meta[idx]
+  #       include = step_meta.get('params').get('include')
+  #       last_state_def_inside_stm = states_def[idx+include]
+  #       transitions_def = last_state_def_inside_stm.get('transitions')
+  #       for tr in transitions_def:
+  #         if tr['event'] == 'next':
+  #           tr['target'] = state_name
+  #   return states_def
--- flow_converter/step_converter.py
+++ flow_converter/step_converter.py
@@ -2,31 +2,32 @@ import copy
 from .templates import Templates 
 
 class StateType():
-  EXEC, STM_END, STM_FORINRANGE, STM_WHILE = range(4)
+  EXEC, STM_BEGIN, STM_END = range(3)
+  # EXEC, STM_BEGIN, STM_END, STM_FORINRANGE, STM_WHILE = range(5)
 
 class StateOffset():
   REARWARD, NO, FORWARD = range(-1,2)
-
-# class StateOrder():
-#   REGULAR, LAST = range(2)
 
 
 class StepConverter():
   def __init__(self, meta):
     self.meta = meta
+    return
+
 
   def convert(self):
     meta = self.meta
     templates = Templates()
     self.states_transitions = templates.get_transitions()
 
-    def step_to_state_def(idx, offset=StateOffset.NO):
+    def step_name_to_state_def_name(idx, offset=StateOffset.NO):
       step_meta = meta[idx+offset]
       if 'exec' in step_meta:
         step_name = step_meta['exec']
       else:
         step_name = step_meta['stm']
       return "{}-{}".format(idx+offset, step_name.split('.')[1]), step_name
+
 
     def converter(idx):
       step_meta = meta[idx]
@@ -35,51 +36,30 @@ class StepConverter():
           return StateType.EXEC
         else:
           stm = step_meta['stm']
+          if stm == 'glbstm.begin':
+            return StateType.STM_BEGIN
           if stm == 'glbstm.end':
             return StateType.STM_END
-          if stm == 'glbstm.forinrangestm':
-            return StateType.STM_FORINRANGE
-          if stm == 'glbstm.whilestm':
-            return StateType.STM_WHILE
       
-      state_entry_action = ''
-      state_exit_action = ''
       state_type = state_type(idx)
       state_transitions = []
-      # for all stms - begin
-      # if state_type == StateType.STM_FORINRANGE:
-      if state_type > StateType.STM_END:
-        step_params = step_meta.get('params')
-        incl = step_params.get('include')
-      # for all stms - end
-        state_transitions = self.states_transitions[StateType.STM_FORINRANGE]
-      else:
-        state_transitions = self.states_transitions[state_type]
-      state_name, act_name = step_to_state_def(idx)
+      state_transitions = self.states_transitions[state_type]
+      state_name, act_name = step_name_to_state_def_name(idx)
+      state_entry_action = ''
+      state_exit_action = ''
+      if idx > 0 and idx < len(meta)-1:
+        # Set an entry action for regular states only
+        state_entry_action = act_name
       trans_def = copy.deepcopy(state_transitions)
       for n, tr in enumerate(trans_def):
         tr['name'] = 'tr' + str(idx) + str(n) 
         tr['src'] = state_name
         event_name = trans_def[n]['event'] 
         if event_name == 'next':
-          tr['action'] = act_name
-          if idx < len(meta)-1:
-            tr['target'], _ = step_to_state_def(idx, StateOffset.FORWARD)
-          else:
-            tr['target'] = state_name
-        elif event_name == 'next_end':
-          tr['action'] = act_name
-          if idx < len(meta)-1:
-            tr['target'], _ = step_to_state_def(idx, StateOffset.FORWARD+incl)
-          else:
-            tr['target'] = state_name
+          tr['target'], _ = step_name_to_state_def_name(idx, StateOffset.FORWARD)
         elif event_name == 'prev':
-          if idx == 0:
-            tr['target'], _ = step_to_state_def(idx)
-          else:
-            tr['target'], _ = step_to_state_def(idx, StateOffset.REARWARD)
+          tr['target'], _ = step_name_to_state_def_name(idx, StateOffset.REARWARD)
         else: # 'current'
-          tr['action'] = act_name
           tr['target'] = state_name
       return state_name, trans_def, state_entry_action, state_exit_action
     return converter    
